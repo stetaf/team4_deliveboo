5852fcedbd9fc9f67a3cae3eff5cbcd85e2c6d1f
stetaf <s.tafuri95@gmail.com>
2021-08-30 15:24:47 +0200

    Create restaurants table migration

---
+++ database/migrations/2021_08_30_131403_create_restaurants_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRestaurantsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id')->after('id');
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('CASCADE');
+            $table->string('name');
+            $table->string('address');
+            $table->string('piva', 11);
+            $table->string('image')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('restaurants', function (Blueprint $table) {
+            $table->dropForeign(['user_id']);
+            $table->dropColumn('user_id');
+        });
+        Schema::dropIfExists('restaurants');
+    }
+}
