0be38f9fb18b12366ac94539a7ab8b035d18beeb
stetaf <s.tafuri95@gmail.com>
2021-08-30 13:02:17 +0200

    Update webpack rules

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,8 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
+    .copyDirectory('resources/img', 'public/img')
+    .options({
+        processCssUrls: false
+    })
     .sass('resources/sass/app.scss', 'public/css');
