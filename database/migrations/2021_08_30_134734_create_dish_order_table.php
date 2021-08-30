5e798d3e962cc4f96550935dfbe0fc76eb26d6b8
stetaf <s.tafuri95@gmail.com>
2021-08-30 15:50:09 +0200

    Create dish_order table migration

---
+++ database/migrations/2021_08_30_134734_create_dish_order_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateDishOrderTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dish_order', function (Blueprint $table) {
+            $table->unsignedBigInteger('order_id');
+            $table->foreign('order_id')->references('id')->on('orders')->onDelete('CASCADE');
+            $table->unsignedBigInteger('dish_id');
+            $table->foreign('dish_id')->references('id')->on('dishes')->onDelete('CASCADE');
+            $table->tinyInteger('quantity');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {      
+        Schema::table('dish_order', function (Blueprint $table) {
+            $table->dropForeign(['order_id']);
+            $table->dropColumn('order_id');
+            $table->dropForeign(['dish_id']);
+            $table->dropColumn('dish_id');
+        });
+        Schema::dropIfExists('dish_order');
+    }
+}
