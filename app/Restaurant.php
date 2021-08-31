cd3243449078016fd0b2a3c9d59bee05c076b660
stetaf <s.tafuri95@gmail.com>
2021-08-31 12:49:57 +0200

    Add restaurant-dish relation method

--- app/Restaurant.php
+++ app/Restaurant.php
@@ -18,4 +18,8 @@ class Restaurant extends Model
     public function user() {
         return $this->belongsTo(User::class);
     }
+
+    public function dish() {
+        return $this->hasMany(Dish::class);
+    }
 }
