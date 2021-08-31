f980ae931b7dd8928e668f223e681741b9244416
stetaf <s.tafuri95@gmail.com>
2021-08-31 11:44:44 +0200

    Create restaurants resource route

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,12 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::prefix('admin')
+      ->namespace('Admin')
+      ->name('admin.')
+      ->middleware('auth')
+      ->group(function() {
+          Route::resource('restaurants', 'UserController');
+});   
