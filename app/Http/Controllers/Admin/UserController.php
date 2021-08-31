1d2d5afff05e650d3431d0096aee79528186814d
stetaf <s.tafuri95@gmail.com>
2021-08-31 11:35:26 +0200

    Create user crud

---
+++ app/Http/Controllers/Admin/UserController.php
@@ -0,0 +1,109 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use App\Restaurant;
+use App\Type;
+
+class UserController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $restaurants = Restaurant::all();
+
+        return view('admin.home', compact('restaurants'));
+    }
+
+ /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        $types = Type::all();
+
+        return view('admin.restaurant.create', compact('types'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'name'    => 'required|max:255',
+            'address' => 'required|max:255',
+            'piva'    => 'required|max:11|min:11',
+            'user_id' => 'exists:users,id',
+            'image'   => 'nullable|image|max:150'
+        ]);       
+
+        $restaurant = Restaurant::create($validated);
+        $restaurant->user()->associate(Auth::user()->id)->save();
+        
+        //$restaurant->types()->attach($request->types);
+
+        return redirect()->route('admin.dashboard')->with('message', "Nuovo ristorante $restaurant->name inserito!");
+    }
+
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Restaurant $restaurant
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Restaurant $restaurant)
+    {
+        return view('admin.restaurant', compact('restaurant'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        /// TO BE CODED AS AN ADDITIONAL FUNCTIONALITY
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        /// TO BE CODED AS AN ADDITIONAL FUNCTIONALITY
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Restaurant $restaurant)
+    {
+        //$restaurant->types()->sync([]);
+        $restaurant->delete();
+
+        return redirect()->route('admin.dashboard')->with('message', "Ristorante $restaurant->name eliminato con successo!");
+    }
+}
