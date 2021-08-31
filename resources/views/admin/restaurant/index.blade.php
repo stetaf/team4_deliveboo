0920c8f89f9e0b79e16da549b5839107547af93b
stetaf <s.tafuri95@gmail.com>
2021-08-31 16:53:47 +0200

    Create single page restaurant structure

--- resources/views/admin/restaurant/index.blade.php
+++ resources/views/admin/restaurant/index.blade.php
@@ -1,59 +1,66 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.admin')
 
-        <title>Laravel</title>
+@section('content')
+    <span>{{ $restaurant->name }}</span>
+    
+    <hr>
+    <!-- DELETE -->
+    <h1>Delete restaurant</h1>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
-    </head>
-    <body>
-    @if ($errors->any())
-    <div class="alert alert-danger">
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-    @endif
+    <form action="{{ Route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
+        @csrf
+        @method('DELETE')
+        <button type="submit" class="btn btn-danger">Confirm</button>
+    </form>
+    <!-- /DELETE -->
 
-        <span>{{ $restaurant->name }}</span>
+    <hr>
+    <!-- NEW -->
+    <H1>New restaurant</H1>
 
-        <form action="{{ Route('admin.restaurants.destroy', $restaurant->id) }}" method="POST">
-            @csrf
-            @method('DELETE')
-            <button type="submit" class="btn btn-danger">Confirm</button>
-        </form>
+    <form action="{{ Route('admin.restaurants.store') }}" method="POST">
+        @csrf
 
-        <form action="{{ Route('admin.restaurants.store') }}" method="POST">
-            @csrf
+        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter the name" value="{{ old('name') }}" required>
+        <input type="text" name="address" class="form-control @error('address') is-invalid @enderror" id="address" placeholder="Enter the address" value="{{ old('address') }}" required>
+        <input type="number" name="piva" class="form-control @error('piva') is-invalid @enderror" id="piva" placeholder="Enter the piva" value="{{ old('piva') }}" required>
 
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter the name" value="{{ old('name') }}" required>
-            <input type="text" name="address" class="form-control @error('address') is-invalid @enderror" id="address" placeholder="Enter the address" value="{{ old('address') }}" required>
-            <input type="number" name="piva" class="form-control @error('piva') is-invalid @enderror" id="piva" placeholder="Enter the piva" value="{{ old('piva') }}" required>
+        <button type="submit" class="btn btn-danger">Confirm</button>
+    </form>
+    <!-- /NEW -->
 
-            <button type="submit" class="btn btn-danger">Confirm</button>
-        </form>
+    <hr>
+    <!-- PIATTI RISTORANTE -->
+    <h1>Piatti</h1>
 
-        <h2>Piatti</h2>
-        <ul>
-            @foreach ($dishes as $dish) 
-                <li>{{ $dish->name }}</li>
-            @endforeach
-        </ul>
+    <ul>
+        @foreach ($dishes as $dish) 
+            <li>
+                {{ $dish->name }}
+                <a href="{{ Route('admin.dish.edit', $dish->id) }}"><input class="btn btn-sm btn-primary ml-4" type="button" value="modifica"></a>
+                <form action="{{ Route('admin.dish.delete', $dish->id) }}" class="d-inline-block" method="post">
+                    @csrf
+                    @method('DELETE')
+                    <input type="submit" class="btn btn-sm btn-danger" value="Elimina">
+                </form>
+            </li>
+        @endforeach
+    </ul>
+    <!-- /PIATTI RISTORANTE -->
 
-        <form action="{{ Route('admin.dish.store', $restaurant->id) }}" method="POST">
-            @csrf
-            <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter the name" value="{{ old('name') }}" required>
-            <input type="text" name="ingredients" class="form-control @error('ingredients') is-invalid @enderror" id="ingredients" placeholder="Enter the ingredients" value="{{ old('ingredients') }}" required>
-            <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" id="description" placeholder="Enter the description" value="{{ old('description') }}" required>
-            <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" id="price" placeholder="Enter the price" value="{{ old('price') }}" required>
-            <input type="number" name="visible" class="form-control @error('visible') is-invalid @enderror" id="visible" placeholder="Enter the visible" value="{{ old('visible') }}" required>
+    <hr>
+    <!-- NUOVO PIATTO RISTORANTE -->
+    <h1>Nuovo piatto</h1>
 
-            <button type="submit" class="btn btn-danger">Confirm</button>
-        </form>
-    </body>
-</html>
+    <form action="{{ Route('admin.dish.store', $restaurant->id) }}" method="POST">
+        @csrf
+        <input type="text" name="name" class="form-control @error('name') is-invalid @enderror" id="name" placeholder="Enter the name" value="{{ old('name') }}" required>
+        <input type="text" name="ingredients" class="form-control @error('ingredients') is-invalid @enderror" id="ingredients" placeholder="Enter the ingredients" value="{{ old('ingredients') }}" required>
+        <input type="text" name="description" class="form-control @error('description') is-invalid @enderror" id="description" placeholder="Enter the description" value="{{ old('description') }}" required>
+        <input type="text" name="price" class="form-control @error('price') is-invalid @enderror" id="price" placeholder="Enter the price" value="{{ old('price') }}" required>
+        <input type="number" name="visible" class="form-control @error('visible') is-invalid @enderror" id="visible" placeholder="Enter the visible" value="{{ old('visible') }}" required>
+
+        <button type="submit" class="btn btn-danger">Confirm</button>
+    </form>
+    <!-- /NUOVO PIATTO RISTORANTE -->
+@endsection
