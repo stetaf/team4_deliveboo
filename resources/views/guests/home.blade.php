f7e08568fcae1756dad67e9e929aa466dc4998a6
stetaf <s.tafuri95@gmail.com>
2021-09-06 11:24:47 +0200

    Make the restaurants cards clickable

--- resources/views/guests/home.blade.php
+++ resources/views/guests/home.blade.php
@@ -39,29 +39,26 @@
             </div>
             <div class="row row-cols-xs-1 row-cols-sm-1 row-cols-md-3 row-cols-lg-4">
                 <div class="col" v-for="restaurant in filtered_results.data" :key="restaurant.id">
-                    <div class="card text-right" style="min-height:255px; position:relative">
-                        <div class="image img-fluid position-relative">
-                            <img src="https://foodies-api.initstore.net/uploads/2.jpg" alt="" class="w-100">
-                            <div class="badges text-right position-absolute" style="right:0px; bottom:0px">
-                                <span class="badge badge-primary m-1" style="background-color: #d3273e; color: #dedede; padding: 4px 6px !important" v-for="type in restaurant.types">@{{ type.name }}</span>
+                    <a :href="/restaurant/ + restaurant.id">
+                        <div class="card text-right card_res" style="min-height:255px; position:relative">
+                            <div class="image img-fluid position-relative">
+                                <img src="https://foodies-api.initstore.net/uploads/2.jpg" alt="" class="w-100">
+                                <div class="badges text-right position-absolute" style="right:0px; bottom:0px">
+                                    <span class="badge badge-primary m-1" style="background-color: #d3273e; color: #dedede; padding: 4px 6px !important" v-for="type in restaurant.types">@{{ type.name }}</span>
+                                </div>
+                            </div>    
+                            <div class="px-1 pt-2" style="min-height: 55px">
+                                <h5 class="font-weight-bold text-center">@{{ restaurant.name }}</h5>
                             </div>
-                        </div>    
-                        <div class="px-1 pt-2" style="min-height: 55px">
-                            <h5 class="font-weight-bold text-center">@{{ restaurant.name }}</h5>
+                            <!-- <div class="cuisine">
+                                <i class="fas fa-bookmark" style="font-size: 60px;color: #0c6f80;transform: rotate(270deg);"></i>
+                                <i class="fas fa-pizza-slice" style="position: absolute;left: 20%;top: 31%; color: white; font-size: 23px;"></i>
+                            </div> -->
+                            <div style="background-color: #dedede" class="p-2 d-flex justify-content-between align-items-center">
+                                <small class="font-italic">@{{ restaurant.address }}</small>
+                            </div>
                         </div>
-                        <!-- <div class="cuisine">
-                            <i class="fas fa-bookmark" style="font-size: 60px;color: #0c6f80;transform: rotate(270deg);"></i>
-                            <i class="fas fa-pizza-slice" style="position: absolute;left: 20%;top: 31%; color: white; font-size: 23px;"></i>
-                        </div> -->
-                        <div style="background-color: #dedede" class="p-2 d-flex justify-content-between align-items-center">
-                            <a :href="/restaurant/ + restaurant.id">
-                                <span class="btn btn-sm text-light" style="background-color: #d3273e">
-                                    Ordina
-                                </span>
-                            </a>
-                            <small class="font-italic">@{{ restaurant.address }}</small>
-                        </div>
-                    </div>
+                    </a>
                 </div>
             </div>
             <pagination :data="filtered_results" @pagination-change-page="getResults" :show-disabled="true" align="center">
