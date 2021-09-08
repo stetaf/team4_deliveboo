74f5ab8ca9ce57faf7a9b2b14aa5569d64ba9b24
stetaf <s.tafuri95@gmail.com>
2021-09-08 16:54:48 +0200

    Fix required fields in checkout page

--- resources/views/guests/restaurant/checkout.blade.php
+++ resources/views/guests/restaurant/checkout.blade.php
@@ -12,23 +12,23 @@
                     <div class="form-row">
                         <div class="form-group col-md-6 mb-0">
                             <label for="customer_name">Nome completo</label>
-                            <input type="text" class="form-control" name="customer_name" placeholder="Nome completo">
+                            <input type="text" class="form-control" name="customer_name" placeholder="Nome completo" value="{{ old('customer_name') }}" required>
                         </div>
                         <div class="form-group col-md-6 mb-0">
                             <label for="customer_email">Email</label>
-                            <input type="email" class="form-control" name="customer_email" placeholder="Email">
+                            <input type="email" class="form-control" name="customer_email" placeholder="Email" value="{{ old('customer_email') }}" required>
                         </div>
                         <div class="form-group col-12 mb-0">
                             <label for="customer_address">Indirizzo</label>
-                            <input type="text" class="form-control" name="customer_address" placeholder="Indirizzo">
+                            <input type="text" class="form-control" name="customer_address" placeholder="Indirizzo" value="{{ old('customer_address') }}" required>
                         </div>
                         <div class="form-group col-12 mb-0">
                             <label for="customer_phone">Telefono</label>
-                            <input type="text" class="form-control" name="customer_phone" placeholder="Telefono">
+                            <input type="number" class="form-control" name="customer_phone" placeholder="Telefono" value="{{ old('customer_phone') }}" pattern="[0-9]+" required>
                         </div>
                         <div class="form-group col-12 mb-0">
                             <label for="notes">Note</label>
-                            <textarea type="text" class="form-control" name="notes" rows="4"></textarea>
+                            <textarea type="text" class="form-control" name="notes" rows="4">{{ old('notes') }}</textarea>
                         </div>
                         <input type="hidden" name="total" :value="cart_total">
                         <div class="form-group col-12 mt-3 mb-0">
