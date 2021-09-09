1aae2079e7c7429f33adbc218b74358dbc4f049d
stetaf <s.tafuri95@gmail.com>
2021-09-09 16:57:20 +0200

    Change checkout page style

--- resources/views/guests/restaurant/checkout.blade.php
+++ resources/views/guests/restaurant/checkout.blade.php
@@ -5,7 +5,54 @@
     @include ('layouts.partials.message')
   @include ('layouts.partials.errors')
         <div class="row">
-            <div class="col-7 py-5">
+        <div class="col-12 col-lg-6 py-5">
+                <h2>Il tuo ordine</h2>
+                <div class="cart_table table-responsive">
+                    <h4 v-if="cart[1].length == 0">Non hai ancora aggiunto nessun piatto!</h4>
+                    <table class="table tbl-cart" v-else>
+                        <thead>
+                            <tr>
+                                <td class="border-top-0 d-none d-md-block">Immagine</td>
+                                <td class="border-top-0">Prodotto</td>
+                                <td class="border-top-0" style="min-width: 85px;">Quantità</td>
+                                <td class="border-top-0" style="min-width: 92px;">Prezzo</td>
+                                <td class="border-top-0"></td>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr v-for="item in cart[1]">
+                                <td class="d-none d-md-block">
+                                    <img :src="'/storage/'+ item.image" :alt="item.name" width="50" height="40">
+                                </td>
+                                <td>
+                                    <span>@{{ item.name }}</span>
+                                </td>
+                                <td>
+                                    <div class="qty">
+                                        <span class="minus" @click="removeItem(item)">
+                                            <i class="fas fa-minus"></i>
+                                        </span>
+                                        <input type="number" class="count border-0 text-center" name="quantity" :value="item.quantity" max="99">
+                                        <span class="plus" @click="addToCart({{$restaurant->id}}, item, 0)">
+                                            <i class="fas fa-plus"></i>
+                                        </span>
+                                    </div>
+                                </td>
+                                <td class="price">
+                                    &euro; @{{ (item.price * item.quantity).toFixed(2) }}
+                                </td>
+                                <td class="text-center">
+                                    <span style="font-size:20px" @click="clearItem(item)">
+                                        <i class="fas fa-trash-alt text-danger"></i>
+                                    </span>
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <span class="text-right d-block font-weight-bold" v-if="cart[1].length > 0">Totale: € @{{ cart_total.toFixed(2) }}</span>
+                </div>
+            </div>
+            <div class="col-12 col-lg-6 py-5">
                 <h2>Inserisci i tuoi dati</h2>
                 <form method="POST" action="{{ Route('guests.restaurant.pay', $restaurant->id) }}">
                     @csrf
@@ -39,59 +86,6 @@
                         </div>
                     </div>
                 </form>
-            </div>
-            <div class="col-5 py-5">
-                <h2>Il tuo ordine</h2>
-                <div class="cart_table table-responsive">
-                    <h4 v-if="cart[1].length == 0">Non hai ancora aggiunto nessun piatto!</h4>
-                    <table class="table tbl-cart" v-else>
-                        <thead>
-                            <tr>
-                                <td class="border-top-0">Immagine</td>
-                                <td class="border-top-0">Prodotto</td>
-                                <td class="border-top-0">Quantità</td>
-                                <td class="border-top-0">Prezzo</td>
-                                <td class="border-top-0"></td>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <tr v-for="item in cart[1]">
-                                <td class="hidden-xs">
-                                    <img :src="'/storage/'+ item.image" :alt="item.name" width="50" height="40">
-                                </td>
-                                <td>
-                                    <span>@{{ item.name }}</span>
-                                </td>
-                                <td>
-                                    <div class="qty">
-                                        <span class="minus" @click="removeItem(item)">
-                                            <i class="fas fa-minus"></i>
-                                        </span>
-                                        <input type="number" class="count border-0 text-center" name="quantity" :value="item.quantity" max="99">
-                                        <span class="plus" @click="addToCart({{$restaurant->id}}, item)">
-                                            <i class="fas fa-plus"></i>
-                                        </span>
-                                    </div>
-                                </td>
-                                <td class="price">
-                                    &euro; @{{ (item.price * item.quantity).toFixed(2) }}
-                                </td>
-                                <td class="text-center">
-                                    <span style="font-size:20px" @click="clearItem(item)">
-                                        <i class="fas fa-trash-alt text-danger"></i>
-                                    </span>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td></td>
-                                <td></td>
-                                <td></td>
-                                <td>Totale: € @{{ cart_total.toFixed(2) }}</td>
-                                <td></td>
-                            </tr>
-                        </tbody>
-                    </table>
-                </div>
             </div>
         </div>
     </div>
